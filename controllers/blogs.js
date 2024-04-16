ba2d47faa6bd6cb7eb4add6f46672d1bbffbe18a
Tommi <tommi.savolainen79@gmail.com>
2024-04-16 10:06:51 +0300

    4.17 tehty

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,19 +2,17 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-blogsRouter.get('', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
+blogsRouter.get('', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+    response.json(blogs);
 });
 
-blogsRouter.delete('/:id', (request, response) => {
-    Blog.findByIdAndDelete(request.params.id).then(() => {
-        response.status(204).end();
-    });
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end();
 });
 
-blogsRouter.put('/:id', (request, response) => {
+blogsRouter.put('/:id', async (request, response) => {
     const body = request.body;
     const blog = {
         title: body.title,
@@ -22,9 +20,8 @@ blogsRouter.put('/:id', (request, response) => {
         url: body.url,
         likes: body.likes,
     };
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true }).then((updatedBlog) => {
-        response.json(updatedBlog);
-    });
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+    response.json(updatedBlog);
 });
 
 blogsRouter.post('/', async (request, response) => {
